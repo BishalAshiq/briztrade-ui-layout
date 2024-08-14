8641bc456aa24b6782ac4164d67d851bb814ecc8
Ashiqul Islam <bishaltheboss20@gmail.com>
2024-08-14 18:43:15 +0600

    working product

--- src/components/home/RFQ.jsx
+++ src/components/home/RFQ.jsx
@@ -2,11 +2,12 @@
 import React, { useRef, useState } from 'react';
 // Import Swiper React components
 import { Swiper, SwiperSlide } from 'swiper/react';
-import { EffectCards, EffectCreative } from 'swiper/modules';
+import { EffectCards, EffectCreative, Autoplay, Pagination, Navigation } from 'swiper/modules';
 // Import Swiper styles
 import 'swiper/css';
 import 'swiper/css/effect-cards';
 import style from "../../app/home/home.module.css";
+import rfqpic from "../../assests/passport.png"
 
 const RFQ = () => {
     return (
@@ -15,8 +16,8 @@ const RFQ = () => {
                 <h1>What is RFQ</h1>
                 <div className='row'>
                     <div className='col-xl-8 col-lg-8 col-md-8 col-sm-12 col-12'>
-
-                        <div className='row'>
+                        <div className={style.vidiorfq}>   
+                            <div className='row'>
                             <div className='col-xl-4 col-lg-4 col-md-4 col-sm-12 col-12'>
                                 <div>
                                     <iframe width="200" height="105" src="https://www.youtube.com/embed/FTH6Dn3AyIQ?si=RNEug663PAHJjFVx" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" referrerpolicy="strict-origin-when-cross-origin" allowfullscreen></iframe>
@@ -47,44 +48,227 @@ const RFQ = () => {
                                     </div>
                                 </div>
                             </div>
-                        </div>
+                        </div></div>
+                     
                     </div>
 
                     <div className='col-xl-4 col-lg-4 col-md-4 col-sm-12 col-12'>
-                        <Swiper
-                            grabCursor={true}
-                            effect={'creative'}
-                            creativeEffect={{
-                                prev: {
-                                    shadow: true,
-                                    translate: [0, 0, -400],
-                                },
-                                next: {
-                                    translate: ['100%', 0, 0],
-                                },
-                            }}
-                            modules={[EffectCreative]}
-                            className="mySwiper"
-                        >
-                            <SwiperSlide>
-                                <div>
-                                    <div className='row'>
-                                        <div className='col-xl-4 col-lg-4 col-md-4'>
+                        <div className={style.rfqcardfull}>
+                            <Swiper
+
+spaceBetween={30}
+centeredSlides={true}
+autoplay={{
+    delay: 5500,
+    disableOnInteraction: false,
+}}
+pagination={{
+    clickable: true,
+    el: `.${style.customPagination}`,
+}}
+navigation={true}
+modules={[Autoplay, Pagination, Navigation]}
+className="mySwiper"
+                            >
+                                <SwiperSlide>
+                                    <div className={style.rfqcard}>
+                                        <div className='row'>
+                                            <div className='col-xl-4 col-lg-4 col-md-4 col-sm-12 col-12'>
+                                                <img className={style.rfqimg} src={rfqpic.src} alt='' />
+                                            </div>
+                                            <div className='col-xl-8 col-lg-8 col-md-8 col-sm-12 col-12'>
+                                                <h6 className={style.textts}>Buyer from the U.S</h6>
+                                                <p className={style.texttss}>Hardware  Computer and Phone
+                                                    Accessories Business</p>
+                                            </div>
+                                            <div className='col-xl-12 col-lg-12 col-md-12 col-sm-12 col-12'>
+                                                <p className={style.texttsss}>   "With RFQ I connected with almost 100
+                                                factories in an instant. Vendor on
+                                                Biztrade reply quickly</p>
+                                             
+                                            </div>
 
                                         </div>
                                     </div>
-                                </div>
 
-                            </SwiperSlide>
-                            <SwiperSlide>Slide 2</SwiperSlide>
-                            <SwiperSlide>Slide 3</SwiperSlide>
-                            <SwiperSlide>Slide 4</SwiperSlide>
-                            <SwiperSlide>Slide 5</SwiperSlide>
-                            <SwiperSlide>Slide 6</SwiperSlide>
-                            <SwiperSlide>Slide 7</SwiperSlide>
-                            <SwiperSlide>Slide 8</SwiperSlide>
-                            <SwiperSlide>Slide 9</SwiperSlide>
-                        </Swiper>
+                                </SwiperSlide>
+                                <SwiperSlide>
+                                    <div className={style.rfqcard}>
+                                        <div className='row'>
+                                            <div className='col-xl-4 col-lg-4 col-md-4 col-sm-12 col-12'>
+                                                <img className={style.rfqimg} src={rfqpic.src} alt='' />
+                                            </div>
+                                            <div className='col-xl-8 col-lg-8 col-md-8 col-sm-12 col-12'>
+                                                <h6 className={style.textts}>Buyer from the U.S</h6>
+                                                <p className={style.texttss}>Hardware  Computer and Phone
+                                                    Accessories Business</p>
+                                            </div>
+                                            <div className='col-xl-12 col-lg-12 col-md-12 col-sm-12 col-12'>
+                                                <p className={style.texttsss}>   "With RFQ I connected with almost 100
+                                                factories in an instant. Vendor on
+                                                Biztrade reply quickly</p>
+                                             
+                                            </div>
+
+                                        </div>
+                                    </div>
+                                </SwiperSlide>
+                                <SwiperSlide>
+                                    <div className={style.rfqcard}>
+                                        <div className='row'>
+                                            <div className='col-xl-4 col-lg-4 col-md-4 col-sm-12 col-12'>
+                                                <img className={style.rfqimg} src={rfqpic.src} alt='' />
+                                            </div>
+                                            <div className='col-xl-8 col-lg-8 col-md-8 col-sm-12 col-12'>
+                                                <h6 className={style.textts}>Buyer from the U.S</h6>
+                                                <p className={style.texttss}>Hardware  Computer and Phone
+                                                    Accessories Business</p>
+                                            </div>
+                                            <div className='col-xl-12 col-lg-12 col-md-12 col-sm-12 col-12'>
+                                                <p className={style.texttsss}>   "With RFQ I connected with almost 100
+                                                factories in an instant. Vendor on
+                                                Biztrade reply quickly</p>
+                                             
+                                            </div>
+
+
+                                        </div>
+                                    </div>
+                                </SwiperSlide>
+                                <SwiperSlide>
+                                    <div className={style.rfqcard}>
+                                        <div className='row'>
+                                            <div className='col-xl-4 col-lg-4 col-md-4 col-sm-12 col-12'>
+                                                <img className={style.rfqimg} src={rfqpic.src} alt='' />
+                                            </div>
+                                            <div className='col-xl-8 col-lg-8 col-md-8 col-sm-12 col-12'>
+                                                <h6 className={style.textts}>Buyer from the U.S</h6>
+                                                <p className={style.texttss}>Hardware  Computer and Phone
+                                                    Accessories Business</p>
+                                            </div>
+                                            <div className='col-xl-12 col-lg-12 col-md-12 col-sm-12 col-12'>
+                                                <p className={style.texttss}>   "With RFQ I connected with almost 100
+                                                factories in an instant. Vendor on
+                                                Biztrade reply quickly</p>
+                                             
+                                            </div>
+
+
+                                        </div>
+                                    </div>
+                                </SwiperSlide>
+                                <SwiperSlide>
+                                    <div className={style.rfqcard}>
+                                        <div className='row'>
+                                            <div className='col-xl-4 col-lg-4 col-md-4 col-sm-12 col-12'>
+                                                <img className={style.rfqimg} src={rfqpic.src} alt='' />
+                                            </div>
+                                            <div className='col-xl-8 col-lg-8 col-md-8 col-sm-12 col-12'>
+                                                <h6 className={style.textts}>Buyer from the U.S</h6>
+                                                <p className={style.texttss}>Hardware  Computer and Phone
+                                                    Accessories Business</p>
+                                            </div>
+                                            <div className='col-xl-12 col-lg-12 col-md-12 col-sm-12 col-12'>
+                                                <p className={style.texttsss}>   "With RFQ I connected with almost 100
+                                                factories in an instant. Vendor on
+                                                Biztrade reply quickly</p>
+                                             
+                                            </div>
+
+
+                                        </div>
+                                    </div>
+                                </SwiperSlide>
+                                <SwiperSlide>
+                                    <div className={style.rfqcard}>
+                                        <div className='row'>
+                                            <div className='col-xl-4 col-lg-4 col-md-4 col-sm-12 col-12'>
+                                                <img className={style.rfqimg} src={rfqpic.src} alt='' />
+                                            </div>
+                                            <div className='col-xl-8 col-lg-8 col-md-8 col-sm-12 col-12'>
+                                                <h6 className={style.textts}>Buyer from the U.S</h6>
+                                                <p className={style.texttss}>Hardware  Computer and Phone
+                                                    Accessories Business</p>
+                                            </div>
+                                            <div className='col-xl-12 col-lg-12 col-md-12 col-sm-12 col-12'>
+                                                <p className={style.texttsss}>   "With RFQ I connected with almost 100
+                                                factories in an instant. Vendor on
+                                                Biztrade reply quickly</p>
+                                             
+                                            </div>
+
+
+                                        </div>
+                                    </div>
+                                </SwiperSlide>
+                                <SwiperSlide>
+                                    <div className={style.rfqcard}>
+                                        <div className='row'>
+                                            <div className='col-xl-4 col-lg-4 col-md-4 col-sm-12 col-12'>
+                                                <img className={style.rfqimg} src={rfqpic.src} alt='' />
+                                            </div>
+                                            <div className='col-xl-8 col-lg-8 col-md-8 col-sm-12 col-12'>
+                                                <h6 className={style.textts}>Buyer from the U.S</h6>
+                                                <p className={style.texttss}>Hardware  Computer and Phone
+                                                    Accessories Business</p>
+                                            </div>
+                                            <div className='col-xl-12 col-lg-12 col-md-12 col-sm-12 col-12'>
+                                                <p className={style.texttsss}>   "With RFQ I connected with almost 100
+                                                factories in an instant. Vendor on
+                                                Biztrade reply quickly</p>
+                                             
+                                            </div>
+
+
+                                        </div>
+                                    </div>
+                                </SwiperSlide>
+                                <SwiperSlide>
+                                    <div className={style.rfqcard}>
+                                        <div className='row'>
+                                            <div className='col-xl-4 col-lg-4 col-md-4 col-sm-12 col-12'>
+                                                <img className={style.rfqimg} src={rfqpic.src} alt='' />
+                                            </div>
+                                            <div className='col-xl-8 col-lg-8 col-md-8 col-sm-12 col-12'>
+                                                <h6 className={style.textts}>Buyer from the U.S</h6>
+                                                <p className={style.texttss}>Hardware  Computer and Phone
+                                                    Accessories Business</p>
+                                            </div>
+                                            <div className='col-xl-12 col-lg-12 col-md-12 col-sm-12 col-12'>
+                                                <p className={style.texttsss}>   "With RFQ I connected with almost 100
+                                                factories in an instant. Vendor on
+                                                Biztrade reply quickly</p>
+                                             
+                                            </div>
+
+
+                                        </div>
+                                    </div>
+                                </SwiperSlide>
+                                <SwiperSlide>
+                                    <div className={style.rfqcard}>
+                                        <div className='row'>
+                                            <div className='col-xl-4 col-lg-4 col-md-4 col-sm-12 col-12'>
+                                                <img className={style.rfqimg} src={rfqpic.src} alt='' />
+                                            </div>
+                                            <div className='col-xl-8 col-lg-8 col-md-8 col-sm-12 col-12'>
+                                                <h6 className={style.textts}>Buyer from the U.S</h6>
+                                                <p className={style.texttss}>Hardware  Computer and Phone
+                                                    Accessories Business</p>
+                                            </div>
+                                            <div className='col-xl-12 col-lg-12 col-md-12 col-sm-12 col-12'>
+                                                <p className={style.texttsss}>   "With RFQ I connected with almost 100
+                                                factories in an instant. Vendor on
+                                                Biztrade reply quickly</p>
+                                             
+                                            </div>
+
+
+                                        </div>
+                                    </div>
+                                </SwiperSlide>
+                            </Swiper>
+                        </div>
                     </div>
                 </div>
             </div>
