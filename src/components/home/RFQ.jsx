5a2a471c290ffaa384e630996c8d91392a98ef9c
Ashiqul Islam <bishaltheboss20@gmail.com>
2024-08-14 20:15:02 +0600

    working product

--- src/components/home/RFQ.jsx
+++ src/components/home/RFQ.jsx
@@ -13,62 +13,63 @@ const RFQ = () => {
     return (
         <div>
             <div className={style.rfqfulldiv}>
-                <h1>What is RFQ</h1>
+                <h1 className={style.rfqtag}>What is RFQ</h1>
                 <div className='row'>
                     <div className='col-xl-8 col-lg-8 col-md-8 col-sm-12 col-12'>
-                        <div className={style.vidiorfq}>   
+                        <div className={style.vidiorfq}>
                             <div className='row'>
-                            <div className='col-xl-4 col-lg-4 col-md-4 col-sm-12 col-12'>
-                                <div>
-                                    <iframe width="200" height="105" src="https://www.youtube.com/embed/FTH6Dn3AyIQ?si=RNEug663PAHJjFVx" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" referrerpolicy="strict-origin-when-cross-origin" allowfullscreen></iframe>
-                                </div>
-                            </div>
-                            <div className='col-xl-8 col-lg-8 col-md-8 col-sm-12 col-12'>
-                                <div>
-                                    <p>Request for Quotation is a free and easy solution for buyers who are seeking quality quotes. By sending out a simple request, you will receive multiple quotes from qualified suppliers</p>
-                                </div>
-                            </div>
-                            <div className='col-xl-12 col-lg-12 col-md-12 col-sm-12 col-12'>
-                                <div className={style.RFQnumbers}>
-                                    <div className={style.RFQnumberofreaching}>
-                                        <span>2,701000+</span>
-                                        <span>RFQs posted</span>
-                                    </div>
-                                    <div className={style.RFQnumberofreaching}>
-                                        <span>163,000++</span>
-                                        <span>Active suppliers</span>
-                                    </div>
-                                    <div className={style.RFQnumberofreaching}>
-                                        <span>22h</span>
-                                        <span>Average response time</span>
-                                    </div>
-                                    <div className={style.RFQnumberofreaching}>
-                                        <span>7602</span>
-                                        <span>Industries covered</span>
+                                <div className='col-xl-4 col-lg-4 col-md-4 col-sm-12 col-12'>
+                                    <div className={style.vidiorfqiframe}>
+                                        <iframe width="200" height="105" src="https://www.youtube.com/embed/FTH6Dn3AyIQ?si=RNEug663PAHJjFVx" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" referrerpolicy="strict-origin-when-cross-origin" allowfullscreen></iframe>
                                     </div>
                                 </div>
-                            </div>
-                        </div></div>
-                     
+                                <div className='col-xl-8 col-lg-8 col-md-8 col-sm-12 col-12'>
+                                    <div>
+                                        <p className={style.moqparagrap}>Request for Quotation is a free and easy solution for buyers who are seeking quality quotes. By sending out a simple request, you will receive multiple quotes from qualified suppliers</p>
+                                    </div>
+                                </div>
+                                
+                                <div className='col-xl-12 col-lg-12 col-md-12 col-sm-12 col-12'>
+                                    <div className={style.RFQnumbers}>
+                                        <div className={style.RFQnumberofreaching}>
+                                            <span className={style.moqNumbers}>2,701000+</span>
+                                            <span className={style.moqNumtext}>RFQs posted</span>
+                                        </div>
+                                        <div className={style.RFQnumberofreaching}>
+                                            <span className={style.moqNumbers}>163,000++</span>
+                                            <span className={style.moqNumtext}>Active suppliers</span>
+                                        </div>
+                                        <div className={style.RFQnumberofreaching}>
+                                            <span className={style.moqNumbers}>22h</span>
+                                            <span className={style.moqNumtext}>Average response time</span>
+                                        </div>
+                                        <div className={style.RFQnumberofreaching}>
+                                            <span className={style.moqNumbers}>7602</span>
+                                            <span className={style.moqNumtext}>Industries covered</span>
+                                        </div>
+                                    </div>
+                                </div>
+                            </div></div>
+
                     </div>
 
                     <div className='col-xl-4 col-lg-4 col-md-4 col-sm-12 col-12'>
                         <div className={style.rfqcardfull}>
                             <Swiper
 
-spaceBetween={30}
-centeredSlides={true}
-autoplay={{
-    delay: 5500,
-    disableOnInteraction: false,
-}}
-pagination={{
-    clickable: true,
-    el: `.${style.customPagination}`,
-}}
-navigation={true}
-modules={[Autoplay, Pagination, Navigation]}
-className="mySwiper"
+                                spaceBetween={30}
+                                centeredSlides={true}
+                                autoplay={{
+                                    delay: 5500,
+                                    disableOnInteraction: false,
+                                }}
+                                pagination={{
+                                    clickable: true,
+                                    el: `.${style.customPagination}`,
+                                }}
+                                navigation={true}
+                                modules={[Autoplay, Pagination, Navigation]}
+                                className="mySwiper"
                             >
                                 <SwiperSlide>
                                     <div className={style.rfqcard}>
@@ -83,9 +84,9 @@ className="mySwiper"
                                             </div>
                                             <div className='col-xl-12 col-lg-12 col-md-12 col-sm-12 col-12'>
                                                 <p className={style.texttsss}>   "With RFQ I connected with almost 100
-                                                factories in an instant. Vendor on
-                                                Biztrade reply quickly</p>
-                                             
+                                                    factories in an instant. Vendor on
+                                                    Biztrade reply quickly</p>
+
                                             </div>
 
                                         </div>
@@ -105,9 +106,9 @@ className="mySwiper"
                                             </div>
                                             <div className='col-xl-12 col-lg-12 col-md-12 col-sm-12 col-12'>
                                                 <p className={style.texttsss}>   "With RFQ I connected with almost 100
-                                                factories in an instant. Vendor on
-                                                Biztrade reply quickly</p>
-                                             
+                                                    factories in an instant. Vendor on
+                                                    Biztrade reply quickly</p>
+
                                             </div>
 
                                         </div>
@@ -126,9 +127,9 @@ className="mySwiper"
                                             </div>
                                             <div className='col-xl-12 col-lg-12 col-md-12 col-sm-12 col-12'>
                                                 <p className={style.texttsss}>   "With RFQ I connected with almost 100
-                                                factories in an instant. Vendor on
-                                                Biztrade reply quickly</p>
-                                             
+                                                    factories in an instant. Vendor on
+                                                    Biztrade reply quickly</p>
+
                                             </div>
 
 
@@ -148,9 +149,9 @@ className="mySwiper"
                                             </div>
                                             <div className='col-xl-12 col-lg-12 col-md-12 col-sm-12 col-12'>
                                                 <p className={style.texttss}>   "With RFQ I connected with almost 100
-                                                factories in an instant. Vendor on
-                                                Biztrade reply quickly</p>
-                                             
+                                                    factories in an instant. Vendor on
+                                                    Biztrade reply quickly</p>
+
                                             </div>
 
 
@@ -170,9 +171,9 @@ className="mySwiper"
                                             </div>
                                             <div className='col-xl-12 col-lg-12 col-md-12 col-sm-12 col-12'>
                                                 <p className={style.texttsss}>   "With RFQ I connected with almost 100
-                                                factories in an instant. Vendor on
-                                                Biztrade reply quickly</p>
-                                             
+                                                    factories in an instant. Vendor on
+                                                    Biztrade reply quickly</p>
+
                                             </div>
 
 
@@ -192,9 +193,9 @@ className="mySwiper"
                                             </div>
                                             <div className='col-xl-12 col-lg-12 col-md-12 col-sm-12 col-12'>
                                                 <p className={style.texttsss}>   "With RFQ I connected with almost 100
-                                                factories in an instant. Vendor on
-                                                Biztrade reply quickly</p>
-                                             
+                                                    factories in an instant. Vendor on
+                                                    Biztrade reply quickly</p>
+
                                             </div>
 
 
@@ -214,9 +215,9 @@ className="mySwiper"
                                             </div>
                                             <div className='col-xl-12 col-lg-12 col-md-12 col-sm-12 col-12'>
                                                 <p className={style.texttsss}>   "With RFQ I connected with almost 100
-                                                factories in an instant. Vendor on
-                                                Biztrade reply quickly</p>
-                                             
+                                                    factories in an instant. Vendor on
+                                                    Biztrade reply quickly</p>
+
                                             </div>
 
 
@@ -236,9 +237,9 @@ className="mySwiper"
                                             </div>
                                             <div className='col-xl-12 col-lg-12 col-md-12 col-sm-12 col-12'>
                                                 <p className={style.texttsss}>   "With RFQ I connected with almost 100
-                                                factories in an instant. Vendor on
-                                                Biztrade reply quickly</p>
-                                             
+                                                    factories in an instant. Vendor on
+                                                    Biztrade reply quickly</p>
+
                                             </div>
 
 
@@ -258,9 +259,9 @@ className="mySwiper"
                                             </div>
                                             <div className='col-xl-12 col-lg-12 col-md-12 col-sm-12 col-12'>
                                                 <p className={style.texttsss}>   "With RFQ I connected with almost 100
-                                                factories in an instant. Vendor on
-                                                Biztrade reply quickly</p>
-                                             
+                                                    factories in an instant. Vendor on
+                                                    Biztrade reply quickly</p>
+
                                             </div>
 
 
