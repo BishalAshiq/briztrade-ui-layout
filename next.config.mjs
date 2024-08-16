546c073eb9faae89e5c81fa04113654e98519cd4
Ashiqul Islam <bishaltheboss20@gmail.com>
2024-08-16 18:03:26 +0600

    working on footer

--- next.config.mjs
+++ next.config.mjs
@@ -1,6 +1,8 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-    
+    images: {
+        domains: ['https://vercel.com/bishalashiqs-projects'], 
+    },
 };
 
 export default nextConfig;
