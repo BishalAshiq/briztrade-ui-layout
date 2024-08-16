c914e98c843b8bedf00393ec34fd154ad31a2898
Ashiqul Islam <bishaltheboss20@gmail.com>
2024-08-16 18:04:16 +0600

    working on footer

--- next.config.mjs
+++ next.config.mjs
@@ -1,7 +1,7 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
     images: {
-        domains: ['https://vercel.com/bishalashiqs-projects'], 
+        domains: ['https://briztrade-ui-layout.vercel.app/'], 
     },
 };
 
