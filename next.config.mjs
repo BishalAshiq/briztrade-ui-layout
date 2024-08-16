e58161a55ac67cd1c0c3d34cba5974c78567c56d
Ashiqul Islam <bishaltheboss20@gmail.com>
2024-08-17 00:15:56 +0600

    SQA

--- next.config.mjs
+++ next.config.mjs
@@ -1,6 +1,9 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
     // output: 'export',
+    images: {
+        domains: ["briztrade-ui-layout.vercel.app"],
+    },
 };
 
 export default nextConfig;
